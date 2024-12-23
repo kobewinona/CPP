3673eea4b3ba2acf3dc8e3552c463daa7f4da73d
dklimkin <d.klimkin@icloud.com>
2024-12-23 15:55:35 +0700

    fix main import error

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -1,4 +1,5 @@
 #include "Array.hpp"
+#include <cstdlib>
 #include <iostream>
 
 #define RESET "\033[0m"
@@ -15,7 +16,7 @@ int main(int, char **)
 	std::cout << "Before modification: ";
 	for (size_t i = 0; i < MAX_VAL; ++i)
 	{
-		intArray[i] = i + 1; // Assign values 1 to 5
+		intArray[i] = i + 1;
 		std::cout << intArray[i] << " ";
 	}
 	std::cout << std::endl;
@@ -23,7 +24,7 @@ int main(int, char **)
 	std::cout << "After modification (increment each value): ";
 	for (size_t i = 0; i < MAX_VAL; ++i)
 	{
-		intArray[i] += 1; // Increment each value by 1
+		intArray[i] += 1;
 		std::cout << intArray[i] << " ";
 	}
 	std::cout << std::endl;
@@ -123,3 +124,58 @@ int main(int, char **)
 
 	return EXIT_SUCCESS;
 }
+
+// #include <iostream>
+// #include "Array.hpp"
+// #include <cstdlib>
+
+// #define MAX_VAL 750
+// int main(int, char **)
+// {
+// 	Array<int> numbers(MAX_VAL);
+// 	int *mirror = new int[MAX_VAL];
+// 	srand(time(NULL));
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		const int value = rand();
+// 		numbers[i] = value;
+// 		mirror[i] = value;
+// 	}
+// 	// SCOPE
+// 	{
+// 		Array<int> tmp = numbers;
+// 		Array<int> test(tmp);
+// 	}
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		if (mirror[i] != numbers[i])
+// 		{
+// 			std::cerr << "didn't save the same value!!" << std::endl;
+// 			return 1;
+// 		}
+// 	}
+// 	try
+// 	{
+// 		numbers[-2] = 0;
+// 	}
+// 	catch (const std::exception &e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+// 	try
+// 	{
+// 		numbers[MAX_VAL] = 0;
+// 	}
+// 	catch (const std::exception &e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		numbers[i] = rand();
+// 	}
+// 	delete[] mirror; //
+// 	return 0;
+// }
