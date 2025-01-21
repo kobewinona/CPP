506a70db823c0599d4cf8b88d001b5791f1cb12b
dklimkin <d.klimkin@icloud.com>
2025-01-21 18:58:15 +0700

    fix ex02

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -1,155 +1,152 @@
 #include "PmergeMe.hpp"
 
 // @defgroup constructors
-PmergeMe::PmergeMe(const std::vector<int> &input) {
-  _vecContainer = input;
-  _deqContainer.assign(input.begin(), input.end());
+PmergeMe::PmergeMe(const std::vector<int> &input)
+{
+	_vecContainer = input;
+	_deqContainer.assign(input.begin(), input.end());
 }
 
 // @def destructor
 PmergeMe::~PmergeMe() {}
+
 // @defgroup member functions
 template <typename Container, typename T>
 typename Container::iterator
-PmergeMe::_binarySearchInsert(Container &container, const T &value,
-                              int &comparisonCount) {
-  typename Container::iterator begin = container.begin();
-  typename Container::iterator end = container.end();
-  typename Container::iterator mid;
+PmergeMe::_binarySearchInsert(Container &container, const T &value)
+{
+	typename Container::iterator begin = container.begin();
+	typename Container::iterator end = container.end();
 
-  while (begin != end) {
-    mid = begin;
-    std::advance(mid, std::distance(begin, end) / 2);
+	while (begin < end)
+	{
+		typename Container::iterator mid = begin;
+		std::advance(mid, std::distance(begin, end) / 2);
 
-    comparisonCount++;
-    if (*mid < value) {
-      ++mid;
-      begin = mid;
-    } else {
-      end = mid;
-    }
-  }
+		if (*mid < value)
+		{
+			begin = mid + 1;
+		}
+		else
+		{
+			end = mid;
+		}
+	}
 
-  return begin; // Position to insert
+	return begin;
 }
 
-std::vector<int> PmergeMe::_generateInsertionOrder(int size) {
-  std::vector<int> order;
-  int bitLength = std::ceil(std::log2(size + 1));
+std::vector<int> PmergeMe::_generateInsertionOrder(int size)
+{
+	std::vector<int> order;
+	int bitLength = std::ceil(log2(size + 1));
 
-  for (int i = 1; i <= size; ++i) {
-    int reversed = 0;
-    for (int j = 0; j < bitLength; ++j) {
-      if (i & (1 << j)) {
-        reversed |= 1 << (bitLength - 1 - j);
-      }
-    }
-    if (reversed > 0 && reversed <= size) {
-      order.push_back(reversed - 1);
-    }
-  }
+	for (int i = 1; i <= size; ++i)
+	{
+		int reversed = 0;
+		for (int j = 0; j < bitLength; ++j)
+		{
+			if (i & (1 << j))
+			{
+				reversed |= 1 << (bitLength - 1 - j);
+			}
+		}
+		if (reversed <= size)
+		{
+			order.push_back(reversed - 1);
+		}
+	}
 
-  return order;
+	return order;
 }
 
 template <typename Container>
-void PmergeMe::_mergeInsertSort(Container &numbers) {
-  size_t n = numbers.size();
-  if (n <= 1) {
-    return;
-  }
+void PmergeMe::_mergeInsertSort(Container &numbers)
+{
+	size_t n = numbers.size();
+	if (n <= 1)
+	{
+		return;
+	}
 
-  // Step 1: Pair elements and split into two groups
-  Container larger, smaller;
+	// Step 1: Pair elements into two groups
+	Container larger, smaller;
 
-  for (size_t i = 0; i + 1 < n; i += 2) {
-    _comparisonsCount++;
-    if (numbers[i] > numbers[i + 1]) {
-      larger.push_back(numbers[i]);
-      smaller.push_back(numbers[i + 1]);
-    } else {
-      larger.push_back(numbers[i + 1]);
-      smaller.push_back(numbers[i]);
-    }
-  }
+	for (size_t i = 0; i + 1 < n; i += 2)
+	{
+		if (numbers[i] > numbers[i + 1])
+		{
+			larger.push_back(numbers[i]);
+			smaller.push_back(numbers[i + 1]);
+		}
+		else
+		{
+			larger.push_back(numbers[i + 1]);
+			smaller.push_back(numbers[i]);
+		}
+	}
 
-  // Handle an odd element
-  if (n % 2 != 0) {
-    smaller.push_back(numbers.back());
-  }
+	// Handle an odd element
+	if (n % 2 != 0)
+	{
+		smaller.push_back(numbers.back());
+	}
 
-  // Step 2: Recursively sort the larger elements
-  _mergeInsertSort(larger);
+	// Step 2: Recursively sort the larger group
+	_mergeInsertSort(larger);
 
-  // Step 3: Start merging into result
-  Container result;
-  result.push_back(larger[0]); // Start with the smallest larger element
+	// Step 3: Merge smaller elements in bit-reversed order
+	Container result;
+	result.insert(result.end(), larger.begin(), larger.end());
 
-  if (!smaller.empty()) {
-    result.insert(result.begin(), smaller[0]); // Insert paired smaller element
-  }
+	std::vector<int> insertionOrder = _generateInsertionOrder(smaller.size());
+	for (size_t i = 0; i < insertionOrder.size(); ++i)
+	{
+		size_t index = insertionOrder[i];
+		typename Container::iterator pos = _binarySearchInsert(result, smaller[index]);
+		result.insert(pos, smaller[index]);
+	}
 
-  // Step 4: Insert remaining smaller elements using bit-reversed order
-  std::vector<int> insertionOrder = _generateInsertionOrder(smaller.size() - 1);
-
-  for (size_t i = 0; i < insertionOrder.size(); ++i) {
-    size_t index = insertionOrder[i];
-    if (index + 1 < smaller.size()) {
-      typename Container::iterator pos =
-          _binarySearchInsert(result, smaller[index + 1], _comparisonsCount);
-      result.insert(pos, smaller[index + 1]);
-    }
-  }
-
-  // Step 5: Insert remaining sorted larger elements
-  for (size_t i = 1; i < larger.size(); ++i) {
-    result.push_back(larger[i]);
-  }
-
-  // Copy sorted result back
-  numbers = result;
+	numbers = result;
 }
 
 template <typename T>
 void PmergeMe::printContainer(const std::string &message,
-                              const T &container) const {
-  std::cout << message;
-  for (typename T::const_iterator it = container.begin(); it != container.end();
-       ++it) {
-    std::cout << *it << " ";
-  }
-  std::cout << std::endl;
+							  const T &container) const
+{
+	std::cout << message;
+	for (typename T::const_iterator it = container.begin(); it != container.end();
+		 ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 }
 
-// Execute the algorithm and measure performance
-void PmergeMe::execute() {
-  clock_t start, end;
+void PmergeMe::execute()
+{
+	clock_t start, end;
 
-  _comparisonsCount = 0;
-  // Sort and time vector
-  start = clock();
-  _mergeInsertSort(_vecContainer);
-  end = clock();
-  double vecTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+	// Sort and time vector
+	start = clock();
+	_mergeInsertSort(_vecContainer);
+	end = clock();
+	double vecTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
 
-  std::cout << std::endl;
-  printContainer("Sorted vector: ", _vecContainer);
-  std::cout << std::fixed;
-  std::cout << "Time taken: " << vecTime << " seconds" << std::endl;
-  std::cout << "Number of comparisons made: " << _comparisonsCount << std::endl;
+	std::cout << std::endl;
+	printContainer("Sorted vector: ", _vecContainer);
+	std::cout << std::fixed;
+	std::cout << "Time taken: " << vecTime << " seconds" << std::endl;
 
-  _comparisonsCount = 0;
+	// Sort and time deque
+	start = clock();
+	_mergeInsertSort(_deqContainer);
+	end = clock();
+	double deqTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
 
-  // Sort and time deque
-  start = clock();
-  _mergeInsertSort(_deqContainer);
-  end = clock();
-  double deqTime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+	std::cout << std::endl;
+	printContainer("Sorted deque: ", _deqContainer);
+	std::cout << "Time taken: " << deqTime << " seconds" << std::endl;
 
-  std::cout << std::endl;
-  printContainer("Sorted deque: ", _deqContainer);
-  std::cout << "Time taken: " << deqTime << " seconds" << std::endl;
-  std::cout << "Number of comparisons made: " << _comparisonsCount << std::endl;
-
-  std::cout << std::endl;
+	std::cout << std::endl;
 }
--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -7,32 +7,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
-// 5 1 2 123 60 70 40
-class PmergeMe {
+class PmergeMe
+{
 private:
-  int _comparisonsCount;
-  std::vector<int> _vecContainer;
-  std::deque<int> _deqContainer;
+	std::vector<int> _vecContainer;
+	std::deque<int> _deqContainer;
 
-  template <typename Container> void _mergeInsertSort(Container &container);
-  template <typename Container, typename T>
-  typename Container::iterator _binarySearchInsert(Container &container,
-                                                   const T &value,
-                                                   int &comparisonCount);
-  std::vector<int> _generateInsertionOrder(int size);
+	template <typename Container>
+	void _mergeInsertSort(Container &container);
+	template <typename Container, typename T>
+	typename Container::iterator _binarySearchInsert(Container &container,
+													 const T &value);
+	std::vector<int> _generateInsertionOrder(int size);
 
 public:
-  PmergeMe();
-  PmergeMe(const std::vector<int> &input);
-  PmergeMe(const PmergeMe &other);
-  PmergeMe &operator=(const PmergeMe &other);
-  ~PmergeMe();
+	PmergeMe();
+	PmergeMe(const std::vector<int> &input);
+	PmergeMe(const PmergeMe &other);
+	PmergeMe &operator=(const PmergeMe &other);
+	~PmergeMe();
 
-  template <typename Container>
-  void printContainer(const std::string &message,
-                      const Container &container) const;
-  void execute();
+	template <typename Container>
+	void printContainer(const std::string &message,
+						const Container &container) const;
+	void execute();
 };
 
 #endif // PMERGE_ME_HPP
